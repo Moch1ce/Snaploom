5dd5aa3fd9746c00dd8edeb5ad202bf323d8022b
刘钏 <liuchuana@hotmail.com>
2026-07-21 23:12:29 +0800

    fix(ci): finalize Swift release checksum (#49)
    Signed-off-by: 刘钏 <liuchuana@hotmail.com>

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let snaploomBinaryChecksum = "d21d3e4e2f6d0f4f78bbe00efe2d1b3e5482df660e44f63122329a59cc23ec63"
+let snaploomBinaryChecksum = "813cd3f55ecb7513f651a4fe07d2ebee2984862edac0e3ea384e4cf122e9c799"
 
 let package = Package(
   name: "SnaploomCapture",
